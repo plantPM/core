fdba179ad323dec0717161a01890314a0ecb81a8
micziz <miczicontent@gmail.com>
2022-04-21 22:31:20 +0200

    Install fix

--- build.sh
+++ build.sh
@@ -1,4 +1,3 @@
-cd src
 gcc main.c -o main
 clear
 ./main
--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 function install() {
     echo "Installing the program"
     curl -sL https://raw.githubusercontent.com/plantPM/core/main/src/main.c
-    curl -sL https://raw.githubusercontent.com/plantPM/core/main/build.sh
+    curl -sL https://raw.githubusercontent.com/plantPM/core/main/build.sh | sh
     chmod +x build.sh
     ./build.sh
     rm build.sh
