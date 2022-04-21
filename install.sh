b6d440eac39971b230fdf37612db90d8e9698504
micziz <miczicontent@gmail.com>
2022-04-21 22:42:09 +0200

    ok shloud work

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 function install() {
     echo "Installing the program"
-    curl -sL -O https://raw.githubusercontent.com/plantPM/core/main/src/main.c
-    curl -sL https://raw.githubusercontent.com/plantPM/core/main/build.sh
+    curl -OsL https://raw.githubusercontent.com/plantPM/core/main/src/main.c
+    curl -OsL https://raw.githubusercontent.com/plantPM/core/main/build.sh
     chmod +x build.sh
     ./build.sh
     rm build.sh
