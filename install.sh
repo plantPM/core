ee7d511ecdb3f193fa7b0e3e5825fdd2f58babd1
micziz <miczicontent@gmail.com>
2022-04-21 22:38:02 +0200

    GO

--- install.sh
+++ install.sh
@@ -1,7 +1,10 @@
 function install() {
     echo "Installing the program"
-    curl -sL https://raw.githubusercontent.com/plantPM/core/main/src/main.c
-    curl -sL https://raw.githubusercontent.com/plantPM/core/main/build.sh | sh
+    curl -sL -O https://raw.githubusercontent.com/plantPM/core/main/src/main.c
+    curl -sL https://raw.githubusercontent.com/plantPM/core/main/build.sh
+    chmod +x build.sh
+    ./build.sh
+    rm main.c
 }
 
 install
