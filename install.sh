9a5a83586a2af449622e40d954cb968cd2d40aec
micziz <miczicontent@gmail.com>
2022-04-21 22:47:00 +0200

    Remove build.sh

--- install.sh
+++ install.sh
@@ -2,9 +2,7 @@ function install() {
     echo "Installing the program"
     curl -OsL https://raw.githubusercontent.com/plantPM/core/main/build.sh
     curl -OsL https://raw.githubusercontent.com/plantPM/core/main/src/main.c
-    chmod +x build.sh
-    ./build.sh
-    rm build.sh
+    gcc main.c -o main
 }
 
 install
