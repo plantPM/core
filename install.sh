ea6722df8ae97f69190c38b45877b2eae3598c9a
micziz <miczicontent@gmail.com>
2022-04-21 22:31:50 +0200

    MORE INSTALL FIX

--- install.sh
+++ install.sh
@@ -2,9 +2,6 @@ function install() {
     echo "Installing the program"
     curl -sL https://raw.githubusercontent.com/plantPM/core/main/src/main.c
     curl -sL https://raw.githubusercontent.com/plantPM/core/main/build.sh | sh
-    chmod +x build.sh
-    ./build.sh
-    rm build.sh
 }
 
 install
