512b538b02a116cbdc84f045c3490deb5df68e5b
micziz <miczicontent@gmail.com>
2022-04-22 00:51:29 +0200

    add comments and modified reamde

--- src/main.c
+++ src/main.c
@@ -1,8 +1,14 @@
+// Include standard libraries
+// stdio.h for printf (and other functions)
+// stdlib.h for running system commands
+// string.h for string functions
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
+// install function
 int install(){
+    // Ask user for package name
     printf("What to install?\n");
     // get input
     char input[100];
