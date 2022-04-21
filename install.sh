7004f02b6c6092a509ea64a229da0a8dc794e760
micziz <miczicontent@gmail.com>
2022-04-21 22:43:18 +0200

    vai

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 function install() {
     echo "Installing the program"
-    curl -OsL https://raw.githubusercontent.com/plantPM/core/main/src/main.c
     curl -OsL https://raw.githubusercontent.com/plantPM/core/main/build.sh
+    curl -OsL https://raw.githubusercontent.com/plantPM/core/main/src/main.c
     chmod +x build.sh
     ./build.sh
     rm build.sh
